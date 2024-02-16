96e9fb27627b635518af5a21d8aa84fa62db9007
Ali Emre Yazıcı <44236466+aeyzc@users.noreply.github.com>
2024-02-17 01:24:15 +0300

    add tryget function

--- HexMap.cs
+++ HexMap.cs
@@ -1,159 +1,226 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public abstract class HexMap : HexMap<int>
+namespace HexSystem
 {
-}
-
-public class HexMap<T>
-{
-    private readonly Dictionary<CubeCoordinate, T> _map;
-
-    public HexMap()
+    public abstract class HexMap : HexMap<int>
     {
-        _map = new Dictionary<CubeCoordinate, T>();
     }
 
-    public void Set(CubeCoordinate coordinate, T obj)
+    public class HexMap<T>
     {
-        _map[coordinate] = obj;
-    }
+        private readonly Dictionary<CubeCoordinate, T> _map;
 
-    public T Get(CubeCoordinate coordinate)
-    {
-        return Get(coordinate.Q, coordinate.R);
-    }
-
-    public T Get(int q, int r)
-    {
-        return _map[new CubeCoordinate(q, r)];
-    }
-
-    public bool TileExists(CubeCoordinate coordinate)
-    {
-        return _map.ContainsKey(coordinate);
-    }
-
-    #region Get Area Functions
-
-    public static List<CubeCoordinate> GetNeighborsOfHex(CubeCoordinate coordinate)
-    {
-        var list = new List<CubeCoordinate>();
-        foreach (var d in CubeCoordinate.Directions) list.Add(coordinate + d);
-        return list;
-    }
-
-    public static List<CubeCoordinate> GetRectangle(CubeCoordinate coordinate, int range)
-    {
-        var list = new List<CubeCoordinate>();
-        for (var q = -range; q <= range; q++)
+        public HexMap()
         {
-            for (var r = -range; r <= range; r++)
+            _map = new Dictionary<CubeCoordinate, T>();
+        }
+
+        public void Set(CubeCoordinate coordinate, T obj)
+        {
+            _map[coordinate] = obj;
+        }
+
+        public T Get(CubeCoordinate coordinate)
+        {
+            return Get(coordinate.Q, coordinate.R);
+        }
+
+        public bool TryGet(CubeCoordinate coordinate, out T value)
+        {
+            if (TileExists(coordinate))
             {
-                var tile = new CubeCoordinate(q, r) + coordinate;
-                list.Add(tile);
+                value = Get(coordinate);
+                return true;
             }
+            value = default;
+            return false;
         }
 
-        return list;
-    }
-
-    public static List<CubeCoordinate> GetLine(CubeCoordinate coordinate, CubeCoordinate direction, int range)
-    {
-        var list = new List<CubeCoordinate>();
-        for (var i = 0; i < range; i++)
+        public List<T> GetAll()
         {
-            var newCoordinate = coordinate + direction * i;
-            list.Add(newCoordinate);
+            return _map.Values.ToList();
         }
 
-        return list;
-    }
-
-    public static List<CubeCoordinate> GetRing(CubeCoordinate center, int radius)
-    {
-        var list = new List<CubeCoordinate>();
-        var hex = center + CubeCoordinate.DirS * radius;
-        for (var i = 0; i < 6; i++)
+        public void Clear()
         {
-            for (var j = 0; j < radius; j++)
+            _map.Clear();
+        }
+
+        private T Get(int q, int r)
+        {
+            return _map[new CubeCoordinate(q, r)];
+        }
+
+        public bool TileExists(CubeCoordinate coordinate)
+        {
+            return _map.ContainsKey(coordinate);
+        }
+
+        #region Get Area Functions
+
+        public static List<CubeCoordinate> GetNeighborsOfHex(CubeCoordinate coordinate)
+        {
+            var list = new List<CubeCoordinate>();
+            foreach (var d in CubeCoordinate.Directions) list.Add(coordinate + d);
+            return list;
+        }
+
+        public static List<CubeCoordinate> GetRectangle(CubeCoordinate coordinate, int range)
+        {
+            var list = new List<CubeCoordinate>();
+            for (var q = -range; q <= range; q++)
             {
-                list.Add(hex);
-                hex += CubeCoordinate.GetDirection(i);
+                for (var r = -range; r <= range; r++)
+                {
+                    var tile = new CubeCoordinate(q, r) + coordinate;
+                    list.Add(tile);
+                }
             }
+
+            return list;
         }
 
-        return list;
-    }
-
-    public static List<CubeCoordinate> GetTriangle(CubeCoordinate center, CubeCoordinate direction, int range)
-    {
-        var list = new List<CubeCoordinate>();
-        var lineDir = CubeCoordinate.RotateDirection(direction, 2);
-        var firstLine = GetLine(center, direction, range);
-        for (var i = 0; i < firstLine.Count; i++)
+        public static List<CubeCoordinate> GetLine(CubeCoordinate coordinate, CubeCoordinate direction, int range)
         {
-            list.AddRange(GetLine(firstLine[i], lineDir, i + 1));
-        }
-
-        return list;
-    }
-
-    public static List<CubeCoordinate> GetArea(CubeCoordinate coordinate, int range)
-    {
-        var list = new List<CubeCoordinate>();
-        for (var x = -range; x <= range; x++)
-        {
-            for (var y = Mathf.Max(-range, -x - range); y <= Mathf.Min(range, -x + range); y++)
+            var list = new List<CubeCoordinate>();
+            for (var i = 0; i < range; i++)
             {
-                var tile = new CubeCoordinate(x, y) + coordinate;
-                list.Add(tile);
+                var newCoordinate = coordinate + direction * i;
+                list.Add(newCoordinate);
             }
+
+            return list;
         }
 
-        return list;
-    }
-
-    #endregion
-
-    #region World Functions
-
-    public static CubeCoordinate WorldPositionToHexCoordinate(Vector3 point, int hexSize = 1)
-    {
-        var qP = Convert.ToInt32((Mathf.Sqrt(3) / 3 * point.x - 1 / 3f * -point.z) / hexSize);
-        var rP = Convert.ToInt32(2 / 3f * -point.z / hexSize);
-        var frac = new CubeCoordinate(qP, rP);
-        var q = Mathf.RoundToInt(frac.Q);
-        var r = Mathf.RoundToInt(frac.R);
-        var s = Mathf.RoundToInt(frac.S);
-        var qDiff = Mathf.Abs(q - frac.Q);
-        var rDiff = Mathf.Abs(r - frac.R);
-        var sDiff = Mathf.Abs(s - frac.S);
-
-        if (qDiff > rDiff && qDiff > sDiff) q = -r - s;
-        else if (rDiff > sDiff) r = -q - s;
-        else s = -q - r;
-
-        return new CubeCoordinate(q, r, s);
-    }
-
-    public static CubeCoordinate WorldPositionToHexCoordinate(Vector2 point, int hexSize = 1)
-    {
-        return WorldPositionToHexCoordinate(new Vector3(point.x, 0, point.y), hexSize);
-    }
-
-    public static Vector3 GetPositionOfHex(CubeCoordinate coordinate, int hexSize = 1)
-    {
-        var r = coordinate.R;
-        var pos = new Vector3
+        public static List<CubeCoordinate> GetRing(CubeCoordinate center, int radius)
         {
-            x = Mathf.Sqrt(3.0f) * (coordinate.Q + r / 2.0f) * hexSize,
-            z = -1.5f * r * hexSize
-        };
+            var list = new List<CubeCoordinate>();
+            var hex = center + CubeCoordinate.DirS * radius;
+            for (var i = 0; i < 6; i++)
+            {
+                for (var j = 0; j < radius; j++)
+                {
+                    list.Add(hex);
+                    hex += CubeCoordinate.GetDirection(i);
+                }
+            }
 
-        return pos;
+            return list;
+        }
+
+        public static List<CubeCoordinate> GetLine(CubeCoordinate center, CubeCoordinate target)
+        {
+            var list = new List<CubeCoordinate>();
+            var distance = CubeCoordinate.Distance(center, target);
+            if (distance < 1) return list;
+            var centerPos = GetPositionOfHex(center);
+            var targetPos = GetPositionOfHex(target);
+            var unitDistance = (targetPos - centerPos) / distance;
+            for (var i = 0; i <= distance; i++)
+            {
+                var pos = centerPos + unitDistance * i;
+                list.Add(WorldPositionToHexCoordinate(pos));
+            }
+
+            return list;
+        }
+
+        public static List<CubeCoordinate> GetTriangle(CubeCoordinate center, CubeCoordinate direction, int range)
+        {
+            var list = new List<CubeCoordinate>();
+            var lineDir = CubeCoordinate.RotateDirection(direction, 2);
+            var firstLine = GetLine(center, direction, range);
+            for (var i = 0; i < firstLine.Count; i++)
+            {
+                list.AddRange(GetLine(firstLine[i], lineDir, i + 1));
+            }
+
+            return list;
+        }
+
+        public static List<CubeCoordinate> GetArea(CubeCoordinate coordinate, int range)
+        {
+            var list = new List<CubeCoordinate>();
+            for (var x = -range; x <= range; x++)
+            {
+                for (var y = Mathf.Max(-range, -x - range); y <= Mathf.Min(range, -x + range); y++)
+                {
+                    var tile = new CubeCoordinate(x, y) + coordinate;
+                    list.Add(tile);
+                }
+            }
+
+            return list;
+        }
+
+        #endregion
+
+        #region World Functions
+
+        public static CubeCoordinate WorldPositionToHexCoordinate(Vector3 point, int hexSize = 1)
+        {
+            float q = (Mathf.Sqrt(3f) / 3f * point.x - 1f / 3f * -point.z) / hexSize;
+            float r = (2f / 3f * -point.z) / hexSize;
+
+            float x = q;
+            float z = r;
+            float y = -x - z;
+
+            int rx = Mathf.RoundToInt(x);
+            int ry = Mathf.RoundToInt(y);
+            int rz = Mathf.RoundToInt(z);
+
+            float xDiff = Mathf.Abs(rx - x);
+            float yDiff = Mathf.Abs(ry - y);
+            float zDiff = Mathf.Abs(rz - z);
+
+            if (xDiff > yDiff && xDiff > zDiff)
+            {
+                rx = -ry - rz;
+            }
+            else if (yDiff > zDiff)
+            {
+                ry = -rx - rz;
+            }
+            else
+            {
+                rz = -rx - ry;
+            }
+
+            return new CubeCoordinate(rx, rz);
+        }
+
+        public static CubeCoordinate WorldPositionToHexCoordinate(Vector2 point, int hexSize = 1)
+        {
+            return WorldPositionToHexCoordinate(new Vector3(point.x, 0, point.y), hexSize);
+        }
+
+        public static Vector3 GetPositionOfHex(CubeCoordinate coordinate, int hexSize = 1)
+        {
+            var r = coordinate.R;
+            var pos = new Vector3
+            {
+                x = Mathf.Sqrt(3.0f) * (coordinate.Q + r / 2.0f) * hexSize,
+                z = -1.5f * r * hexSize
+            };
+
+            return pos;
+        }
+
+        public static Vector2 GetPositionOfHex2D(CubeCoordinate coordinate, int hexSize = 1)
+        {
+            var r = coordinate.R;
+            var pos = new Vector2
+            {
+                x = Mathf.Sqrt(3.0f) * (coordinate.Q + r / 2.0f) * hexSize,
+                y = -1.5f * r * hexSize
+            };
+
+            return pos;
+        }
+
+        #endregion
     }
-
-    #endregion
 }
